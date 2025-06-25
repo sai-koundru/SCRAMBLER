cb1e607d4a2c562574361ae87763d61045506a19
K SREE SAI VENKAT <2003ssv@gmail.com>
2025-06-25 14:11:13 +0530

    Update tb_scrambler.v

--- tb_scrambler.v
+++ tb_scrambler.v
@@ -1,11 +1,11 @@
-module tb_chat_test;
+module tb_scrambler;
 
     reg clk;
     reg [7:0] din;
     
     wire [7:0] scrambler_out;
 
-    chat_test uut (  .clk(clk), .din(din),  .scrambler_out(scrambler_out) );
+    chat_test uut ( .clk(clk), .din(din), .scrambler_out(scrambler_out) );
 
     
     initial clk = 0;
@@ -14,11 +14,11 @@ module tb_chat_test;
     
     initial 
         begin
-                        din = 8'b10101001;
-                #10000;
-       
-        $finish;
-       
+            
+            din = 8'b10101001;
+            #10000;
+            $finish;
+            
         end
 
 endmodule
