c6e993ef1a829640465bcd0bc1ce2ad634d02113
K SREE SAI VENKAT <2003ssv@gmail.com>
2025-06-25 14:04:20 +0530

    Add files via upload

---
+++ tb_scrambler.v
@@ -0,0 +1,24 @@
+module tb_chat_test;
+
+    reg clk;
+    reg [7:0] din;
+    
+    wire [7:0] scrambler_out;
+
+    chat_test uut (  .clk(clk), .din(din),  .scrambler_out(scrambler_out) );
+
+    
+    initial clk = 0;
+    always #15 clk = ~clk;
+
+    
+    initial 
+        begin
+                        din = 8'b10101001;
+                #10000;
+       
+        $finish;
+       
+        end
+
+endmodule
